105d711ab2e089a5c89fd1269be55588dd2a9511
neo1224 <nnballarta@up.edu.ph>
2025-12-11 20:41:49 +0800

    connect the backend and frontend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to the API" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
