0250e5b0c2d4305ea15a25ec4772a63685d2773a
neo1224 <nnballarta@up.edu.ph>
2025-12-18 11:06:11 +0800

    Setup Prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import type { Request, Response } from "express";
+import express from 'express';
+import type { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!');
 });
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Welcome to the API" });
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.json({ message: 'Welcome to the API' });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+   console.log(`Server is running at http://localhost:${PORT}`);
 });
